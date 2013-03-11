4bf70094fdaab93a4347fa62610c5d87e05ede7c
Christian Wehner <christian.wehner@gcsc.uni-frankfurt.de>
2013-03-11 19:01:30 +0000

    Script adjusted so that different discretization types can be tested (fvcr,fv1,fe).

--- navierstokesdirichlet.lua
+++ navierstokesdirichlet.lua
@@ -27,10 +27,13 @@ dim = util.GetParamNumber("-dim", 2) -- default dimension is 2.
 
 -- chose "fvcr" or "stabil"
 discType = util.GetParam("-type", "fvcr")
+order = util.GetParamNumber("-order", 1)
+vorder = util.GetParamNumber("-vorder", order)
+porder = util.GetParamNumber("-porder", order-1)
 
 InitUG(dim, AlgebraType("CPU", 1));
 
-R=1000
+R=100
 
 if 	dim == 2 then
 gridName = util.GetParam("-grid", "unit_square_01/unit_square_01_tri_2x2.ugx")
@@ -47,6 +50,8 @@ print("    numTotalRefs = " .. numRefs)
 print("    numPreRefs 	= " .. numPreRefs)
 print("    type       = " .. discType)
 print("    grid       	= " .. gridName)
+print("    v ansatz order = " ..vorder)
+print("    p ansatz order = " ..porder)
 
 --------------------------------------------
 --------------------------------------------
@@ -67,16 +72,28 @@ dom = util.CreateAndDistributeDomain(gridName, numRefs, numPreRefs, requiredSubs
 -- All subset are ok. So we can create the Approximation Space
 approxSpace = ApproximationSpace(dom)
 
+-- we destinguish the components of the velocity 
+if 		dim == 1 then VelCmp = {"u"}; FctCmp = {"u", "p"};
+elseif  dim == 2 then VelCmp = {"u", "v"}; FctCmp = {"u", "v", "p"};
+elseif  dim == 3 then VelCmp = {"u", "v", "w"}; FctCmp = {"u", "v", "w", "p"};
+else print("Choosen Dimension " .. dim .. "not supported. Exiting."); exit(); end
+
 if discType=="fvcr" then
 	if dim >= 1 then approxSpace:add_fct("u", "Crouzeix-Raviart") end
 	if dim >= 2 then approxSpace:add_fct("v", "Crouzeix-Raviart") end
 	if dim >= 3 then approxSpace:add_fct("w", "Crouzeix-Raviart") end
 	approxSpace:add_fct("p", "piecewise-constant") 
-else
-	if dim >= 1 then approxSpace:add_fct("u", "Lagrange", 1) end
-	if dim >= 2 then approxSpace:add_fct("v", "Lagrange", 1) end
-	if dim >= 3 then approxSpace:add_fct("w", "Lagrange", 1) end
-	approxSpace:add_fct("p", "Lagrange", 1)
+end
+if discType=="fv1" then
+	approxSpace:add_fct(FctCmp, "Lagrange", 1) 
+end
+if discType=="fv" or discType=="fe" then
+	approxSpace:add_fct(VelCmp, "Lagrange", vorder)
+	if porder==0 then
+		approxSpace:add_fct("p", "piecewise-constant") 
+    else 
+		approxSpace:add_fct("p", "Lagrange", porder) 
+	end
 end
 
 -- finally we print some statistic on the distributed dofs
@@ -103,22 +120,24 @@ if discType=="fvcr" then
 	noUpwind = NavierStokesNoUpwind();
 	fullUpwind = NavierStokesFullUpwind();
 	NavierStokesDisc:set_conv_upwind(fullUpwind)
-	
-else
-	--upwind = NavierStokesNoUpwind();
-	--upwind = NavierStokesFullUpwind();
-	--upwind = NavierStokesSkewedUpwind();
-	upwind = NavierStokesLinearProfileSkewedUpwind();
+	NavierStokesDisc:set_defect_upwind(false);
+	NavierStokesDisc:set_defect_upwind(true);
+end	
+if discType=="fv1" then
+	upwind = NavierStokesNoUpwind();
+	-- upwind = NavierStokesFullUpwind();
+	-- upwind = NavierStokesSkewedUpwind();
+	-- upwind = NavierStokesLinearProfileSkewedUpwind();
 	--upwind = NavierStokesRegularUpwind();
 	--upwind = NavierStokesPositiveUpwind();
 	-- chose stabilization 
 	stab = NavierStokesFIELDSStabilization()
-	--stab = NavierStokesFLOWStabilization()
+	stab = NavierStokesFLOWStabilization()
 	-- ... and set the upwind
 	stab:set_upwind(upwind)
 	--stab:set_diffusion_length("RAW")
-	stab:set_diffusion_length("FIVEPOINT")
-	--stab:set_diffusion_length("COR")
+	--stab:set_diffusion_length("FIVEPOINT")
+	stab:set_diffusion_length("COR")
 	
 	-- set stabilization
 	NavierStokesDisc:set_stabilization(stab)
@@ -133,8 +152,7 @@ NavierStokesDisc:set_exact_jacobian(false)
 NavierStokesDisc:set_stokes(false)
 NavierStokesDisc:set_laplace(true)
 NavierStokesDisc:set_kinematic_viscosity(1.0/R);
-NavierStokesDisc:set_defect_upwind(false);
-NavierStokesDisc:set_defect_upwind(true);
+
 
 
 ----------------------------------
@@ -182,10 +200,10 @@ InletDisc:add("inletVel"..dim.."d", "Boundary")
 
 function source2d(x, y, t)
 	return 
-	3*x*x+
+--	3*x*x+ -- dx p term
 (-4*y+12*x^2+12*y^2-24*x^3-8*y^3+24*x*y-72*x*y^2+48*x*y^3-48*x^2*y+72*x^2*y^2-48*x^2*y^3+48*x^3*y-24*x^4*y+12*x^4-16*x^3*y^3*R-20*x^4*y^2*R+36*x^5*y^2*R+28*x^3*y^4*R-28*x^6*y^2*R-24*x^3*y^5*R+80*x^4*y^3*R-140*x^4*y^4*R+120*x^4*y^5*R-144*x^5*y^3*R+252*x^5*y^4*R-216*x^5*y^5*R+112*x^6*y^3*R-32*x^7*y^3*R+8*x^7*y^2*R-196*x^6*y^4*R+168*x^6*y^5*R-40*x^4*y^6*R+72*x^5*y^6*R-56*x^6*y^6*R+56*x^7*y^4*R-48*x^7*y^5*R+16*x^7*y^6*R+8*x^3*y^6*R+4*x^3*y^2*R)/R
 	,
-	3*y*y+
+--	3*y*y+ -- dy p term
 (4*x-12*x^2-12*y^2+8*x^3+24*y^3-24*x*y+48*x*y^2-48*x*y^3+72*x^2*y-72*x^2*y^2-48*x^3*y+48*x^3*y^2+24*x*y^4-12*y^4-16*x^3*y^3*R+80*x^3*y^4*R-144*x^3*y^5*R+28*x^4*y^3*R-140*x^4*y^4*R+252*x^4*y^5*R-24*x^5*y^3*R+120*x^5*y^4*R-216*x^5*y^5*R+8*x^6*y^3*R-40*x^6*y^4*R+72*x^6*y^5*R-196*x^4*y^6*R+168*x^5*y^6*R-56*x^6*y^6*R+112*x^3*y^6*R-20*x^2*y^4*R+36*x^2*y^5*R-28*x^2*y^6*R-32*x^3*y^7*R+56*x^4*y^7*R-48*x^5*y^7*R+16*x^6*y^7*R+8*x^2*y^7*R+4*x^2*y^3*R)/R
 end
 
@@ -237,12 +255,12 @@ vankaBase:set_convergence_check(baseConvCheck)
 crilut = CRILUT()
 crilut:set_threshold(1e-1,1e-4,1e-4,1e-4)
 crilut:set_damp(1)
--- CRILUT:set_info(true)
+-- crilut:set_info(true)
 ilutBase = ILUT()
-ilutBase:set_threshold(0)
+ilutBase:set_threshold(1e-4)
 ilutBase:set_info(false)
 ilutSolver = LinearSolver()
-ilutSolver:set_preconditioner(crilut)
+ilutSolver:set_preconditioner(ilutBase)
 ilutSolver:set_convergence_check(ConvCheck(100000, 5e-8, 1e-1, false))
 
 
@@ -250,12 +268,20 @@ gmg = GeometricMultiGrid(approxSpace)
 gmg:set_discretization(domainDisc)
 gmg:set_base_level(0)
 gmg:set_base_solver(ilutSolver)
-gmg:set_smoother(crilut)
+if discType=="fv1" then
+	ilut = ILUT()
+	ilut:set_threshold(1e-3)
+	ilut:set_info(false)
+	gmg:set_smoother(ilut)
+end
+if discType=="fvcr" then
+	gmg:set_smoother(vanka)
+end
 gmg:set_cycle_type(1)
 gmg:set_num_presmooth(1)
 gmg:set_num_postsmooth(1)
 gmg:set_damp(MinimalResiduumDamping())
-gmg:add_prolongation_post_process(AverageComponent(approxSpace,"p"))
+-- gmg:add_prolongation_post_process(AverageComponent(approxSpace,"p"))
 -- gmg:set_damp(MinimalEnergyDamping())
 
 --gmg:set_debug(dbgWriter)
@@ -270,25 +296,29 @@ gmgSolver:set_convergence_check(ConvCheck(10000, 1e-7, 1e-2, true))
 
 
 -- choose a solver
-if discType=="stabil" then
---	solver = linSolver
-else
-	solver = vankaSolver
+if discType=="fvcr" or discType=="fv1" then
+	solver = gmgSolver
 end
-solver = gmgSolver
--- solver = ilutSolver
--- solver = BiCGStabSolver
--- solver = vankaSolver
+if discType=="fe" or discType=="fv" then
+	if porder==0 then
+		solver = vankaSolver
+	else
+		solver = LU()
+	end
+end
 
 newtonConvCheck = ConvCheck()
 newtonConvCheck:set_maximum_steps(100)
 newtonConvCheck:set_minimum_defect(1e-7)
-newtonConvCheck:set_reduction(1e-10)
--- newtonConvCheck:set_reduction(0.9999999)
+if discType=="fv" or discType=="fe" then
+	newtonConvCheck:set_reduction(0.999)
+else 
+	newtonConvCheck:set_reduction(1e-10)
+end
 newtonConvCheck:set_verbose(true)
 
 newtonLineSearch = StandardLineSearch()
-newtonLineSearch:set_maximum_steps(20)
+newtonLineSearch:set_maximum_steps(30)
 newtonLineSearch:set_lambda_start(1.0)
 newtonLineSearch:set_reduce_factor(0.85)
 newtonLineSearch:set_accept_best(true)
@@ -303,8 +333,6 @@ newtonSolver:set_line_search(newtonLineSearch)
 newtonSolver:set_debug(dbgWriter)
 
 tBefore = os.clock()
-
-NavierStokesDisc:set_conv_upwind(noUpwind)
 
 newtonSolver:init(op)
 
@@ -324,55 +352,6 @@ l2error = L2Error(vSolution, u, "v", 0.0, 1, "Inner")
 write("L2Error in v component is "..l2error .."\n");
 l2error = L2Error(pSolution, u, "p", 0.0, 1, "Inner")
 write("L2Error in p component is "..l2error .."\n");
-
-NavierStokesDisc:set_conv_upwind(noUpwind);
-
-source = SeparatedPressureSource(approxSpace,u)
-source:set_source(rhs)
-NavierStokesDisc:set_source(source)
-source:update()
-Interpolate("StartValue_p", u, "p")
-
-if newtonSolver:prepare(u) == false then
-	print ("Newton solver prepare failed."); exit();
-end
-
-if newtonSolver:apply(u) == false then
-	 print ("Newton solver apply failed."); exit();
-end
-
-l2error = L2Error(uSolution, u, "u", 0.0, 1, "Inner")
-write("L2Error in u component is "..l2error .."\n");
-l2error = L2Error(vSolution, u, "v", 0.0, 1, "Inner")
-write("L2Error in v component is "..l2error .."\n");
-l2error = L2Error(pSolution, u, "p", 0.0, 1, "Inner")
-write("L2Error in p component is "..l2error .."\n");
-
-
--- Interpolate("StartValue_u", u, "u")
--- Interpolate("StartValue_v", u, "v")
--- Interpolate("StartValue_p", u, "p")
-	
-source = SeparatedPressureSource(approxSpace,u);
-source:set_source(rhs)
-NavierStokesDisc:set_source(source)
--- Interpolate("StartValue_p", u, "p")
-	
-	for i=1,0 do
-
-		source:update()
-		Interpolate("StartValue_p", u, "p")
-		if newtonSolver:apply(u) == false then
-			print ("Newton solver apply failed."); exit();
-		end	
-	end
-
--- l2error = L2Error(uSolution, u, "u", 0.0, 1, "Inner")
--- write("L2Error in u component is "..l2error .."\n");
--- l2error = L2Error(vSolution, u, "v", 0.0, 1, "Inner")
--- write("L2Error in v component is "..l2error .."\n");
--- l2error = L2Error(pSolution, u, "p", 0.0, 1, "Inner")
--- write("L2Error in p component is "..l2error .."\n");
 
 
 out = VTKOutput()
