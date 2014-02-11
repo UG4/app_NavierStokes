ced5134d34a2afe11463b64c56955ec7835025b8
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2014-02-11 13:08:36 +0000

    allowing grouping of components in gnuplot-error-measure

--- dirichlet.lua
+++ dirichlet.lua
@@ -341,6 +341,32 @@ end
 if not(bInstat) then
 
 	if bConvRates then
+		
+		local options = {	
+		
+			size = 				{12.5, 6.75}, -- the size of canvas (i.e. plot)
+			sizeunit =			"cm", -- one of: cm, mm, {in | inch}, {pt | pixel}
+			font = 				"Arial",
+			fontsize =			12,
+			
+			logscale = 			true,
+			grid = 				"lc rgb 'grey70' lt 0 lw 1", 
+			linestyle =			{colors = gnuplot.RGBbyLinearHUEandLightness(8, 1, 360+40, 85, 0.4, 0.4), 
+								linewidth = 3, pointsize = 1.3},
+			border = 			" back lc rgb 'grey40' lw 2",
+			decimalsign = 		",",
+			key =	 			"on box lc rgb 'grey40' right top Left reverse spacing 2 width 1.1 samplen 2 height 0.5",
+			tics =	 			{x = "nomirror out scale 0.75 format '%g' font ',8'",
+								 y = "10 nomirror out scale 0.75 format '%.te%01T' font ',8'"}, 
+			mtics =				5,
+			slope = 			{dy = 3, quantum = 0.25, at = "last"},
+			padrange = 			{ x = {0.8, 4}, y = {0.5, 1.5}},
+		}
+	
+		if util.HasParamOption("-replot") then
+			util.rates.static.replot(options); exit()
+		end
+	
 		util.rates.static.compute(
 		{
 			ExactSol = {
@@ -354,6 +380,9 @@ if not(bInstat) then
 				["p"] = "pgrad"..dim.."d"
 			},
 			
+			PlotCmps = { v = {"u","v"}, p = {"p"}},
+			MeasLabel = function (disc, p) return disc.." $\\mathbb{Q}_{"..p.."}/\\mathbb{Q}_{"..(p-1).."}$" end,
+			
 			CreateDomain = CreateDomain,
 			CreateApproxSpace = CreateApproxSpace,
 			CreateDomainDisc = CreateDomainDisc,
@@ -363,8 +392,11 @@ if not(bInstat) then
 			
 			DiscTypes = 
 			{
-			  {type = "fv", pmin = 2, pmax = 4, lmin = 1, lmax = numRefs}
-			}
+			  {type = "fv", pmin = 2, pmax = 4, lmin = 2, lmax = numRefs}
+			},
+			
+			gpOptions = options,
+			noplot = true,
 		})
 	end
 	
